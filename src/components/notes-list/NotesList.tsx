49cbe334d93e4920ccd548e16ec45f42e01620d5
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2023-09-29 16:40:55 +0300

    added NotesList component

---
+++ src/components/notes-list/NotesList.tsx
@@ -0,0 +1,21 @@
+'use-client'
+
+import styles from '@/components/note-list/NoteList.module.scss'
+import { NotesListProps } from '@/types/note-list-props.interface'
+import NoteItem from '../note-item/NoteItem'
+
+const NotesList: React.FC<NotesListProps> = ({ notes, onDelete }) => {
+	return (
+		<div className={styles.notesWrapper}>
+			{notes.map(note => (
+				<NoteItem
+					key={note.dateOfCreation.toString()}
+					note={note}
+					onDelete={onDelete}
+				/>
+			))}
+		</div>
+	)
+}
+
+export default NotesList
