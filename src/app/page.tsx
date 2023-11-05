66cbde4e15287e04ad964948bb125bd389453bfe
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2023-11-05 12:44:35 +0200

    removed comment in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,8 @@ import NotesList from '@/components/notes-list/NotesList'
 import PrimaryTitle from '@/components/ui/primary-title/PrimaryTitle'
 import useNotes from '@/hooks/useNotes'
 import styles from './page.module.scss'
+
 export default function Home() {
-	/*const [notes, setNotes] = useState<INote[]>([])
-
-	useEffect(() => {
-		// this part will be refactored in lab3
-		const storedNotes = localStorage.getItem('notes')
-		if (storedNotes) {
-			const parsedNotes: INote[] = JSON.parse(storedNotes)
-
-			const convertedNotes = parsedNotes.map(note => ({
-				...note,
-				dateOfCreation: new Date(note.dateOfCreation),
-			}))
-			setNotes(convertedNotes)
-		} else {
-			const initialNotes: INote[] = [
-				{
-					title: 'First note',
-					text: 'deserunt mollit anim id est laborum....',
-					dateOfCreation: new Date(),
-					category: NoteCategory.Thanksgiving,
-				},
-			]
-			setNotes(initialNotes)
-		}
-	}, [])
-
-	useEffect(() => {
-		localStorage.setItem('notes', JSON.stringify(notes))
-	}, [notes])
-*/
 	const { notes, setNotes, loading, error } = useNotes()
 
 	const deleteNote = (dateOfCreation: Date) => {
