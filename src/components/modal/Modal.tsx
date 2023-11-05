cbf8f969afdc700096f1f381579a2565d877a166
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2023-11-05 13:02:33 +0200

    added useRef

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,19 +3,29 @@ import React, { FC, PropsWithChildren } from 'react'
 import './Modal.scss'
 
 interface ModalProps {
-    isActive: boolean;
-    setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+	isActive: boolean
+	setIsActive: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, isActive, setIsActive }) => {
-    return (
-        <div className={isActive ? 'modal active' : 'modal'} onClick={() => setIsActive(false)}>
-            <div className={'content'} onClick={e => e.stopPropagation()}>
-                <button onClick={() => setIsActive(false)} className="close-button">X</button>
-                {children}
-            </div>
-        </div>
-    )
+const Modal: FC<PropsWithChildren<ModalProps>> = ({
+	children,
+	isActive,
+	setIsActive,
+}) => {
+	return (
+		<div
+			className={isActive ? 'modal active' : 'modal'}
+			onClick={() => setIsActive(false)}>
+			<div className={'content'} onClick={e => e.stopPropagation()}>
+				<button
+					onClick={() => setIsActive(false)}
+					className='close-button'>
+					X
+				</button>
+				{children}
+			</div>
+		</div>
+	)
 }
 
 export default Modal
