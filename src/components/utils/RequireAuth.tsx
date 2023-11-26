518fc9a94225d3f5f492c4e5d8319fdb156412c0
Vladyslav Geyna <vladgeina@gmail.com>
2023-11-26 22:12:44 +0200

    Add HOC to wrap pages with required OR NOT required user auth

---
+++ src/components/utils/RequireAuth.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'next/navigation'
+import { FC, PropsWithChildren } from 'react'
+import { useTypedSelector } from '../../hooks/useTypedSelector'
+import Loader from '../ui/loader/Loader'
+
+const RequireAuth: FC<PropsWithChildren> = ({ children }) => {
+	const { isAuthenticated, isLoading, isCheckingAuthFinished } =
+		useTypedSelector(state => state.user)
+
+	if (isLoading || !isCheckingAuthFinished) {
+		return <Loader text='Loading...' />
+	}
+
+	if (!isAuthenticated && isCheckingAuthFinished) {
+		redirect('/login')
+	}
+
+	return children
+}
+
+export default RequireAuth
