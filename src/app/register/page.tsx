4e56451341e7c68ad38a65aced87656f181b7ed7
Vladyslav Geyna <vladgeina@gmail.com>
2023-11-27 20:05:23 +0200

    Made register page available only for not authenticated users

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,18 @@
 import RegistrationForm from '@/components/registration-form/RegistrationForm'
 import PrimaryTitle from '@/components/ui/primary-title/PrimaryTitle'
+import RequireNotAuth from '@/components/utils/RequireNotAuth'
 import styles from './page.module.scss'
 
 const page = () => {
 	return (
-		<div>
-			<PrimaryTitle className={styles.title}>Registration</PrimaryTitle>
-			<RegistrationForm />
-		</div>
+		<RequireNotAuth>
+			<div>
+				<PrimaryTitle className={styles.title}>
+					Registration
+				</PrimaryTitle>
+				<RegistrationForm />
+			</div>
+		</RequireNotAuth>
 	)
 }
 
